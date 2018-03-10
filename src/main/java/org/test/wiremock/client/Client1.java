4079b6ccbd9ca8545cb86186acf13cb10173e6db
Baptiste <baptiste.meynier@gmail.com>
2018-03-10 18:31:35 +0100

    some begining tests

---
+++ src/main/java/org/test/wiremock/client/Client1.java
@@ -0,0 +1,20 @@
+package org.test.wiremock.client;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Invocation;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+public class Client1 {
+	
+	public Response getUsers() {
+		
+		Client client = ClientBuilder.newClient();
+		WebTarget webTarget = client.target("http://localhost:8089/rest/api/users");
+		Invocation.Builder invocationBuilder = webTarget.request(MediaType.APPLICATION_JSON);
+		return invocationBuilder.get();
+	}
+
+}
